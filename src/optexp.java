f5baaa046c531086bc2890ca063ba1b4df54582e
abul <abul@192.168.10.52>
2018-08-28 12:03:06 +0100

    pushed

---
+++ src/optexp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class optexp {
+ public static void main(String args[]) {
+  int a, b;
+  a = 10;
+  b = (a == 1) ? 40 : 60;
+  System.out.println("Value of b is : " + b);
+  b = (a == 10) ? 20 : 40;
+  System.out.println("Value of b is : " + b);
+ }
+}
